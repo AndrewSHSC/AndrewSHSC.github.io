385cb539ec752fe0ac409dc037330a2783d136b7
andy2 <andy.shaw@screenmedia.co.uk>
2023-01-04 09:13:35 +0000

    ba

---
+++ GPS/scripts/views/banners/homepageBanner.js
@@ -0,0 +1,52 @@
+(function () {
+  const firstSlide = document.querySelectorAll(".homepageBanner .slide")[0];
+  var videos = document.querySelectorAll(".homepageBanner video");
+
+  videos.forEach(function (video) {
+    var dataSrc = video.getAttribute("data-src");
+    var source = document.createElement("source");
+    source.setAttribute("src", dataSrc);
+    source.removeAttribute("data-src");
+    video.appendChild(source);
+  });
+
+  var video = firstSlide.querySelectorAll("video")[0];
+  if (video) {
+    video.pause();
+    video.currentTime = 0;
+    video.play();
+  }
+
+  setTimeout(function () {
+    const firstLoad = document.querySelectorAll(".homepageBanner.firstLoad")[0];
+    firstLoad.classList.remove("firstLoad");
+    firstSlide.nextElementSibling.classList.add("slide-isActive");
+
+    window.setInterval(activeSlide, 6000);
+  }, 6000);
+
+  function activeSlide() {
+    var activeSlide = document.querySelectorAll(
+      ".homepageBanner .slide-isActive"
+    )[0];
+
+    if (activeSlide != null) {
+      activeSlide.classList.remove("slide-isActive");
+      var nextSlide =
+        activeSlide.nextElementSibling != null
+          ? activeSlide.nextElementSibling
+          : firstSlide;
+
+      if (nextSlide != null) {
+        nextSlide.classList.add("slide-isActive");
+
+        var video = nextSlide.querySelectorAll("video")[0];
+        if (video) {
+          video.pause();
+          video.currentTime = 0;
+          video.play();
+        }
+      }
+    }
+  }
+})();
