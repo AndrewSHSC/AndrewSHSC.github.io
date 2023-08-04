3aa2c9d5f51646549a848ceebd68637a6dcdb157
andy2 <andy.shaw@screenmedia.co.uk>
2023-08-04 13:56:31 +0100

    calc

---
+++ componentCalculator/component-calc-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ItemComponent @item-added="addItemToBasket" />
+    <BasketComponent :basketItems="basketItems" :componentName="componentName" />
+  </div>
+</template>
+
+<script>
+import ItemComponent from './components/ItemComponent.vue';
+import BasketComponent from './components/BasketComponent.vue';
+
+export default {
+  components: {
+    ItemComponent,
+    BasketComponent,
+  },
+  data() {
+    return {
+      basketItems: [],
+      componentName: '',
+    };
+  },
+  methods: {
+    addItemToBasket(item) {
+      this.basketItems.push(item);
+      this.componentName = '';
+    },
+  },
+};
+</script>
